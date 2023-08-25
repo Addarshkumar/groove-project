4b145983f65c771ffff6a452d98599b9c5e4347f
Addarshkumar <addarsh.kumar@kalvium.community>
2023-08-25 19:01:41 +0530

    groove

---
+++ app.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("buy-now");
+var thanks = document.getElementById("thankyou")
+button.onclick=()=>{
+    button.innerHTML="Thank You For Shopping With Us."
+    button.style.backgroundColor="white"
+    button.style.border="none"
+}
